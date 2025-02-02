a3a6f22cca905780b4665744f77ff86450faf15d
pdsl2005 <pdsl2005@gmail.com>
2025-02-02 17:31:44 -0500

    Adds CSV files, panda dataframes for each. Adds LLM/AWS Integration Plan

---
+++ backend/insurance_databases.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+ma_plans = pd.read_csv('backend/CSVs/MassachusettsSBEPUF2024/MAPlans05102024.csv')
+ct_plans = pd.read_csv('backend/CSVs/ConnecticutSBEPUF2024/CTPlans05102024.csv')
+me_plans = pd.read_csv('backend/CSVs/MaineSBEPUF2024/MEPlans05102024.csv')
+vt_plans = pd.read_csv('backend/CSVs/VermontSBEPUF2024/VTPlans05102024.csv')
+ri_plans = pd.read_csv('backend/CSVs/RhodeIslandSBEPUF2024/RIPlans05102024.csv')
+
+print(ma_plans.head())
