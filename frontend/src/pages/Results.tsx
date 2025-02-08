9b740b134781ac9de587e9101fb360e5628b477b
lyannne <xu.ly@northeastern.edu>
2025-02-08 01:04:14 -0500

    results text boxes added (rough draft)

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import TextBox from '../components/ResultsText';
 
 const Results: React.FC = () => (
     <div>
         <h1>Results page</h1>
+        <TextBox 
+        title="#1: Plan A" 
+        content="Details" 
+        width= {500}
+        height="250px" 
+        />
+        <h1></h1>
+        <TextBox 
+        title="#2: Plan B" 
+        content="More details" 
+        width="500px" 
+        height="200px" 
+/>
     </div>
+    
 );
 
 export default Results;
