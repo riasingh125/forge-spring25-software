cafd61cf71b6092b18d5da69a04d2face372e1ee
lyannne <xu.ly@northeastern.edu>
2025-02-22 14:27:50 -0500

    results backend connected

--- frontend/src/pages/Results/Results.tsx
+++ frontend/src/pages/Results/Results.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import TextBox from '../../components/ResultsText';
+import { ResultsProps, Result } from '../../App';
 
-const Results: React.FC = () => (
-    <div>
-        <h1>Results page</h1>
-        <TextBox 
-        title="#1: Plan A" 
-        content="Details" 
-        width= {500}
-        height="250px" 
-        />
-        <h1></h1>
-        <TextBox 
-        title="#2: Plan B" 
-        content="More details" 
-        width="500px" 
-        height="200px" 
-/>
-    </div>
+
     
-);
+
+const Results: React.FC<ResultsProps> = ({results, setResults}) => {
+
+    function displayResult(results: Result) {
+        return (
+            <>
+            <TextBox 
+            title={results.name}
+            content={`price: ${results.price}`}
+            width= "500px"
+            height="250px" 
+            />
+            <br></br>
+            </>
+        )
+    }
+
+    return (
+        <>
+            {
+            results.map((result) => (
+                displayResult(result)
+            ))
+            }
+        </>
+
+    )
+    
+    
+}
 
 export default Results;
