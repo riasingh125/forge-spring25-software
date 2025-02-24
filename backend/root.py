dafea420f40951fa8658466f9bf6ed106eade1d4
wongcolin45 <wongcolin45@gmail.com>
2025-02-24 09:30:36 -0500

    stuff

--- backend/root.py
+++ backend/root.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 from rankings import RankingLogic
+
+from models import UserInputForm
+from models import ChatBotMessage
+
 app = FastAPI()
 
 
 # To run app:
 # fastapi dev root.py
 
-# Format for messages sent - a single string
-class ChatBotMessage(BaseModel):
-    message: str
 
 
 @app.get("/")
@@ -27,10 +28,11 @@ def root():
 async def send_message(data: ChatBotMessage):
     return {"received": data.message, "response": "hi"}
 
+
 # Endpoint for sending user input from form
 # 'data' parameters corresponds to JSON body in POST request
 @app.post("/form/send")
-async def send_form(data):
+async def send_form(data: UserInputForm):
     # Adds more stuff to actually recommend.
     form_data = data.model_dump()
 
