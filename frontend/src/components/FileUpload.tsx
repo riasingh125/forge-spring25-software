f01fb0e9a0160f824e4fb20138a255fc520e8cff
narang-an <narang.an@northeastern.edu>
2025-03-10 20:08:44 -0400

    changed handlefilechange so that new files are appended onto the list of old files instead of replacing it

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 
 const FileUpload = () => {
-    const [files, setFiles] = useState<File[]>([]); // Define state as an array of File objects
+  const [files, setFiles] = useState<File[]>([]); // Define state as an array of File objects
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedFiles = event.target.files ? Array.from(event.target.files) : [];
-        setFiles(selectedFiles);
-    };
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newFiles = event.target.files ? Array.from(event.target.files) : [];
+    setFiles((prevFileList) => [...prevFileList, ...newFiles]);
+    console.log(newFiles);
+  };
 
-    return (
-        <div>
-            <input 
-            type="file" 
-            accept="application/pdf"
-            multiple 
-            onChange={handleFileChange} />
-            <menu>
-                {files.map((file, index) => (
-                    <li key={index}>{file.name}</li>
-                ))}
-            </menu>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        type="file"
+        accept="application/pdf"
+        multiple
+        onChange={handleFileChange}
+      />
+      <menu>
+        {files.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </menu>
+    </div>
+  );
 };
 
 export default FileUpload;
