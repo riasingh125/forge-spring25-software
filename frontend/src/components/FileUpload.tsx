529103fc7d4b97352d10d7db9aa6624b7122bb1a
narang-an <narang.an@northeastern.edu>
2025-03-23 18:22:29 -0400

    sends file data to backend hopefully

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -1,30 +1,18 @@
 import React, { useState } from "react";
 import styles from "./FileUploadStyles.module.css";
 
-const FileUpload = () => {
-  const [files, setFiles] = useState<File[]>([]);
+interface FileUploadProps {
+  files: File[];
+  setFiles: React.Dispatch<React.SetStateAction<File[]>>;
+}
 
+const FileUpload: React.FC<FileUploadProps> = ({ files, setFiles }) => {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newFiles = event.target.files ? Array.from(event.target.files) : [];
     setFiles((prevFileList) => [...prevFileList, ...newFiles]);
     console.log(newFiles);
   };
 
-    return (
-        <div>
-            <input 
-            type="file" 
-            accept="application/pdf"
-            multiple 
-            required
-            onChange={handleFileChange} />
-            <menu>
-                {files.map((file, index) => (
-                    <li key={index}>{file.name}</li>
-                ))}
-            </menu>
-        </div>
-    );
   return (
     <div>
       <input
