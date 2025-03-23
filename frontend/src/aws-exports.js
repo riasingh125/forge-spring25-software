2a6359dee34f8f0e78116f09b4031d3258ef2f2d
pon-et <145276703+pon-et@users.noreply.github.com>
2025-03-23 17:32:05 -0400

    setting up app.tsx

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,10 @@
+const awsconfig = {
+    Auth: {
+        region: "us-east-1", 
+        userPoolId: "us-east-1_0BnGQZ2AO", 
+        userPoolWebClientId: "7357poqc90amf4c1c7t3i6dqjs", 
+        mandatorySignIn: false,
+    }, 
+}; 
+
+export default awsconfig; 
