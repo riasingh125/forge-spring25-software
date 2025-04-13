7cd8bc98cf6f616839e2a515bc0c1a59c79b2fa1
wongcolin45 <wongcolin45@gmail.com>
2025-04-13 17:48:36 -0400

    done

--- backend/API/root.py
+++ backend/API/root.py
@@ -20,12 +20,12 @@ app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
-	CORSMiddleware,	#type: ignore
-	allow_origins=["http://localhost:5173"],
-	# Allow frontend in React to connect
-	allow_credentials=True,
-	allow_methods=["*"],  # Allow all methods
-	allow_headers=["*"],  # Allow all headers
+    CORSMiddleware,	#type: ignore
+    allow_origins=["http://localhost:5173"],
+    # Allow frontend in React to connect
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
 )
 
 # To run app:
@@ -43,10 +43,9 @@ def root():
 
 @app.get("/session")
 def get_session_id():
-	session_id = str(uuid4())
-	chat_history[session_id] = {}  # Optional: initialize chat history
-	print("session id made: "+session_id)
-	return {"sessionId" : session_id}
+    session_id = str(uuid4())
+    chat_history[session_id] = {}  # Optional: initialize chat history
+    return {"sessionId" : session_id}
 
 @app.post("/chat/message/{session_id}")
 async def send_message(session_id: str, data: ChatBotMessage):
@@ -56,88 +55,91 @@ async def send_message(session_id: str, data: ChatBotMessage):
 
 @app.post("/form/submit/{session_id}")
 async def upload_pdfs(session_id: str, form_data: str = Form(...),
-					  files: List[UploadFile] = File(...)):
-	# Process form data into dictionary
-	form_dict = json.loads(form_data)
+                      files: List[UploadFile] = File(...)):
 
-	# Check against Pydantic Model
-	user_input = UserInputForm(**form_dict)
-	user_input = user_input.model_dump()
+    form_dict = json.loads(form_data)
+    # Check against Pydantic Model
+    user_input = UserInputForm(**form_dict)
+    user_input = user_input.model_dump()
 
-	# check files for right type:
-	for file in files:
-		if file.content_type != 'application/pdf':
-			return {"error": "file is not of type pdf"}
+    # check files for right type:
+    for file in files:
+        if file.content_type != 'application/pdf':
+            return {"error": "file is not of type pdf"}
 
-	plans = {}
-	premiums = user_input['premium']
-	for i, j in zip(premiums, files):
-		plans[j] = i
+    plans = {}
+    premiums = user_input['premium']
+    for i, j in zip(premiums, files):
+        plans[j] = i
 
-	# upload to s3 and textract
-	# { "name": filename, "text": "TEXT RESULTS " }
-	results = await upload_and_extract(plans)
-
-
-	# The weights:
-	weight = user_input['weights']
-	del user_input['weights']
-
-	coverage = user_input['coverage']
-	location = user_input['address']
-
-	user_input['health_concerns'] = coverage['personal_health_concerns']
-	user_input['budget'] = coverage['budget']
-	user_input['zip_code'] = location['zip_code']
-	user_input['city'] = location['city']
-	user_input['state'] = location['state']
-
-	weights = {k: v/10 for k, v in weight.items()}
-
-	# The premiums:
-	async def process_plan(plan_name: str, plan_content: str, plan_premium: float):
-		ranking_instance = WeightedPlanRanking(weights, plan_content,
-											   user_input, plan_premium)
-		unweighted_scores = await ranking_instance.ranking_logics()
-		weighted_scores = ranking_instance.pair_keys()
-		total_score = ranking_instance.total_scores()
-		summary = PlanSummaries(plan_content, user_input['age'], user_input['budget'])
-		short_summary = await summary.get_short_summary()
-
-		return plan_name, unweighted_scores, weighted_scores, total_score, short_summary, plan_content
-
-	tasks = [process_plan(name, content[0], content[1]) for name, content in results.items()]
-
-	# Run all tasks concurrently
-	results = await asyncio.gather(*tasks)
-
-	to_frontend = []
-
-	prompt = ""
-
-	# Store the results in the history
-	for name, unweighted_scores, weighted_scores, total_score, short_summary, plan_content in results:
-		# {'file_name': 'weighted_scores: dict, 'total_score': float, 'text': str}
-		history[name] = {
-			"weighted_scores": weighted_scores,
-			"total_score": total_score,
-			"text": plan_content
-		}
-
-		to_frontend.append({
-			"name": name,
-			"weightedScores": weighted_scores,
-			"totalScore": total_score,
-			"shortSummary": short_summary
-		})
-		prompt += f"\n\nPlan Name: {name}\nTotal Score: {total_score}\nPlan Text: {short_summary}\n"
-
-	prompt += ("Please justify the ranking of each plan based on its strengths and weaknesses using the information above "
-			   "Explain each plan clearly, highlighting what makes it better or worse compared to the others.\n")
-
-	session_history = chat_history.setdefault(session_id, {})
-	session_history["prompt"] = prompt
+    # upload to s3 and textract
+    # { "name": filename, "text": "TEXT RESULTS " }
+    results = await upload_and_extract(plans)
 
 
-	return to_frontend
+    # The weights:
+    weight = user_input['weights']
+    del user_input['weights']
+
+    coverage = user_input['coverage']
+    location = user_input['address']
+
+    user_input['health_concerns'] = coverage['personal_health_concerns']
+    user_input['budget'] = coverage['budget']
+    user_input['zip_code'] = location['zip_code']
+    user_input['city'] = location['city']
+    user_input['state'] = location['state']
+
+    weights = {k: v/10 for k, v in weight.items()}
+
+    # The premiums:
+    async def process_plan(plan_name: str, plan_content: str,
+                           plan_premium: float):
+        ranking_instance = WeightedPlanRanking(weights, plan_content,
+                                               user_input, plan_premium,
+                                               user_input['individual_bool'])
+        unweighted_scores = await ranking_instance.ranking_logics()
+        weighted_scores = ranking_instance.pair_keys()
+        total_score = ranking_instance.total_scores()
+        summary = PlanSummaries(plan_content, user_input['age'],
+                                user_input['budget'])
+        short_summary = await summary.get_short_summary()
+        return plan_name, unweighted_scores, weighted_scores, total_score, short_summary, plan_content
+
+    tasks = [process_plan(name, content[0], content[1]) for name, content in
+             results.items()]
+
+    # Run all tasks concurrently
+    results = await asyncio.gather(*tasks)
+
+
+    to_frontend = []
+
+    prompt = ""
+
+    # Store the results in the history
+    for name, unweighted_scores, weighted_scores, total_score, short_summary, plan_content in results:
+        # {'file_name': 'weighted_scores: dict, 'total_score': float, 'text': str}
+        history[name] = {
+            "weighted_scores": weighted_scores,
+            "total_score": total_score,
+            "text": plan_content
+        }
+
+        to_frontend.append({
+            "name": name,
+            "weightedScores": weighted_scores,
+            "totalScore": total_score,
+            "shortSummary": short_summary
+        })
+        prompt += f"\n\nPlan Name: {name}\nTotal Score: {total_score}\nPlan Text: {short_summary}\n"
+
+    prompt += ("Please justify the ranking of each plan based on its strengths and weaknesses using the information above "
+               "Explain each plan clearly, highlighting what makes it better or worse compared to the others.\n")
+
+    session_history = chat_history.setdefault(session_id, {})
+    session_history["prompt"] = prompt
+
+
+    return to_frontend
 
