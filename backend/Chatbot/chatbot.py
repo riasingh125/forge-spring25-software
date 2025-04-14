30c3011966c89880b1d477702c8299ebeab74bc0
wongcolin45 <wongcolin45@gmail.com>
2025-04-14 16:23:23 -0400

    done

--- backend/Chatbot/chatbot.py
+++ backend/Chatbot/chatbot.py
@@ -18,18 +18,24 @@ def get_chatbot_response(question: str, history: dict) -> str:
 
 
     # Prompting type
-    prompt = f"""You are a healthcare plan assistant. Only use the text provided to answer user questions.
+    prompt = f"""You are a healthcare plan assistant.
 
     Below is the text for multiple plans:
     {combined_plans_text}
     
-    Answer the following question ONLY from the text above:
+    Answer the following question using the primarily the text above, but guess the answer if you have to :
     
     Question: {question}
+    
+    Try not to response with "I don't know" or "The test doesn't state" or something similar. Use outside context if the answer isn't present in the provided text.
     """
 
     response = client.models.generate_content(
         model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=f"You are a healthcare assistant. Have a casual customer service tone. Try your best to answer each question accurately.",
+            temperature=0.5
+        ),
         contents=[prompt],
     )
 
